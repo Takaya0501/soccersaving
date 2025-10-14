cf46b62997c072a97c7a6c1576eef161dc23dd75
Takaya Okumura <19210@toba-cmt.ac.jp>
2025-10-14 11:54:25 +0300

    change config package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // `sqlite3`のようなネイティブモジュールがビルド時に正しく扱われるようにする設定
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'sqlite3': 'commonjs sqlite3',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
